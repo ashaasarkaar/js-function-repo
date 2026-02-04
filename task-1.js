b6aecad1dea55bcc636d8b17fb385f5bd7a53103
ashaasarkaar <ashaasarkaar@gmail.com>
2026-02-05 00:26:53 +0600

    multiply 4 numbers

---
+++ task-1.js
@@ -0,0 +1,12 @@
+// ### Task-1  
+// Take four parameters. Multiply the four numbers and then return the result 
+
+function multiply(a, b, c, d){
+    const multiply = a * b * c *d;
+    return multiply;
+}
+
+const result = multiply(4, 2, 3, 2)
+console.log(result)
+
+
